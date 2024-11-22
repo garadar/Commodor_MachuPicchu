1eafa8de27429feb06311c04aadbc14aa26a68f1
Victor Férat <victor.ferat@live.Fr>
2024-11-22 20:27:40 +0100

    test

--- script/generate_script.py
+++ script/generate_script.py
@@ -7,8 +7,8 @@ def _generate_script(data, output_dir, template_env):
     manifest = template.render(data)
     # Écrire le résultat dans un fichier de sortie
     output_name = 'script.sh.erb'
-    output_path = os.path.join(output_dir, data['application']['name'], output_name)
-    os.makedirs(os.path.dirname(output_path), 'template', exist_ok=True)
+    output_path = os.path.join(output_dir, data['application']['name'], 'template', output_name)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
     with open(output_path, "w") as f:
         f.write(manifest)
